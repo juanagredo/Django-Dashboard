a548e87292eabfeaa697a648daca4e19bcd4a9c5
juanagredo <Juanagredo.dev@gmail.com>
2025-04-22 09:18:58 -0500

    Add dashboard components and update views for alerts and todo items - Implemented Alert and ToDoItem models with validation.

--- dashboardapp/models.py
+++ dashboardapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Alert:
+    def __init__(self, alert_type, title, lot_number):
+        if alert_type not in ("violation", "service request"):
+            raise ValueError("alert_type must be either 'violation' or 'service request'")
+        
+        self.alert_type = alert_type
+        self.title = title
+        self.lot_number = lot_number
+
+class ToDoItem:
+    def __init__(self, id, title, due, completed=False):
+        self.id = id
+        self.title = title
+        self.due = due
+        self.completed = completed
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'due': self.due,
+            'completed': self.completed,
+        }
--- dashboardapp/views.py
+++ dashboardapp/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from .models import Alert, ToDoItem
 
 def index(request):
-    return render(request, 'dashboard/index.html')
 
+  alerts = [
+        Alert(alert_type="violation", title="Broken Fence", lot_number=101),
+        Alert(alert_type="service request", title="Leaking Pipe", lot_number=204),
+        Alert(alert_type="violation", title="Noise Complaint", lot_number=312),
+        Alert(alert_type="violation", title="Update violation", lot_number=1655)
+    ]
+  
+  todos = [
+        ToDoItem(1,"Prepare docs for meeting", "2 DAYS", False),
+        ToDoItem(2,"Send mail to Paul", "1 DAY", True),
+        ToDoItem(3, "Review Q2 financial report", "3 DAYS", False),
+        ToDoItem(4, "Schedule team check-in", "TODAY", False),
+        ToDoItem(5, "Fix login bug on dashboard", "1 DAY", True),
+        ToDoItem(6, "Prepare slides for stakeholder meeting", "5 DAYS", False),
+    ]
+  
+  todos_data = [todo.to_dict() for todo in todos]
+  
+  context = {
+        "alerts": alerts,
+        'todos': todos_data
+    }
+  return render(request, 'dashboard/index.html', context)
